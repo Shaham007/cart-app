80d719cc6d9a5ef47aa0cbf53aa6d6a3356b5328
Shaha007 <Shaham.prince56@gmail.com>
2022-01-31 16:24:05 +0100

    initial  commit

---
+++ src/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import bag from "./media/icons/bag.svg";
+
+const Navbar = () => {
+  
+    return (
+        <nav>
+            <div className="nav-center">
+                <h3>hotDeals</h3>
+                <div className="nav-container">
+                    <img src={bag} alt="bag" />
+                    <div className="amount-container">
+                        <p className="total-amount">5</p>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
